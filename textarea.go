d3231994c6eca51854d0355b29ad46e2e8fad548
donomii <donomii@gmail.com>
2021-10-02 20:50:56 +0200

    Revert to standard gl library

--- textarea.go
+++ textarea.go
@@ -479,7 +479,7 @@ func (t *TextArea) CopySelection() bool {
 
 func (t *TextArea) PasteFromClipboard() {
 	sc := t.FindWindow().Parent().(*Screen)
-	str := sc.GLFWWindow().GetClipboardString()
+	str, _ := sc.GLFWWindow().GetClipboardString()
 	runes := []rune(str)
 	t.valueTemp = append(t.valueTemp[:t.cursorPos], append(runes, t.valueTemp[t.cursorPos:]...)...)
 	t.cursorPos += len(runes)
